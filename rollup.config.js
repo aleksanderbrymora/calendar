29ff698681321481181a6b9d4f551b9419d9583e
aleksanderbrymora <aleksander.australia@gmail.com>
2021-09-08 09:44:30 +0200

    added test app

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 import pkg from './package.json';
 
@@ -13,6 +16,11 @@ export default {
       strict: false,
     },
   ],
-  plugins: [typescript()],
+  plugins: [
+    json(),
+    commonjs(),
+    nodeResolve({ preferBuiltins: true }),
+    typescript(),
+  ],
   external: ['react', 'react-dom'],
 };
---
+++ test-app/src/App.tsx
@@ -0,0 +1,11 @@
+import Calendar from 'calendar';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
